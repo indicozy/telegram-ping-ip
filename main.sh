dd37982a591213c5b1faf9d35eadbdfe729cd5dc
Indicozy <indicozy@protonmail.com>
2021-10-11 14:46:50 +0600

    First Commit

---
+++ main.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+_ip=`curl ifconfig.me 2> /dev/null`
+test_ip=`cat ip.txt`
+token=`cat token.txt`
+chat_id=`cat chat_id.txt`
+message="IP has changed. now it is $_ip"
+
+echo $_ip
+
+if [ $_ip != $test_ip ]; then
+  echo "Not Equal, tested at `date`"
+  telegram -t "$token" -c "$chat_id" "$message"
+  echo $_ip > ip.txt
+else
+  echo "Equal, tested at `date`"
+fi
+
